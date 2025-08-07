9c60fb0897249e412bab6777bd24bbefa67b3378
Julek <miljulekps@gmail.com>
2025-08-07 06:55:17 +0200

    new

--- client.py
+++ client.py
@@ -1,5 +1,30 @@
 import socket
+import threading
 
-c = socket.socket()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 12345))
 
-c.connect(('localhost', 12345))
+nickname = input("Choose your nickname: ")
+
+def receive():
+    while True:
+        try: 
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                pass
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+           
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+
+        receive_thread = threading.Thread(target=receive)
+        receive_thread.start()
+
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
